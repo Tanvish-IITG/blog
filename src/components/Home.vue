375f56742546c141542af92d040ed5a05328f497
Tanvish <tanvish2001@gmail.com>
2022-05-20 18:40:37 +0530

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,50 @@
+<template>
+    <h1> Hello from home component </h1>
+    <p> Written by {{ MyName }}</p>
+    <input v-model="text" >
+    <p> You have written {{ text }}</p>
+    <span v-for="word in listofwords.entries()" :key="word" style="width:100%">
+        <span :id="color[word[0] % 5]">
+            {{ word[1] + " " }}
+        </span>
+    </span>
+</template>
+
+<script>
+
+export default {
+  name: 'MyHome',
+  data()
+  {
+    return{
+    MyName: "Tanvish",
+    text: "",
+    index: 0,
+    color: ["red","blue","orange","green","yellow"]
+    }
+  },
+  computed:{
+    listofwords(){
+        return this.text.split(" ");
+    },
+  }
+}
+</script>
+
+<style>
+#red{
+    color: red
+}
+#yellow{
+    color: yellow
+}
+#green{
+    color:green
+}
+#blue{
+    color:blue
+}
+#orange{
+    color:orange
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import MyHome from './components/Home.vue'
+
+createApp(App).mount('#app')
+createApp(MyHome).mount('#Home')
